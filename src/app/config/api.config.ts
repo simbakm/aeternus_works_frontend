d90caeedb8f429c968ec9cdd331ad58aafd05472
simbakm <simbarashesandi@gmail.com>
2026-06-26 16:52:52 +0200

    pointing

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -4,7 +4,9 @@
 import { url } from "node:inspector";
 
 // You can also override at runtime by setting `window.__env = { API_URL: '...' }` before the app loads.
-export const API_URL: string = (typeof window !== 'undefined' && (window as any).__env && (window as any).__env.API_URL) || 'http://localhost:8080/api';
+export const API_URL: string = (typeof window !== 'undefined' && (window as any).__env && (window as any).__env.API_URL) || 'https://aeternus-works-back-end.onrender.com/api';
 
 // live url
 // https://aeternus-works-back-end.onrender.com/api
+//local url
+//http://localhost:8080/api
